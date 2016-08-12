fa6745e4625c36064b8738f5b910a8c9e13d0b59
Tolokoban <fabien.petitjean@free.fr>
2016-08-12 13:28:59 +0200

    Publish

--- js/@index.js
+++ js/@index.js
@@ -60,7 +60,7 @@ addListener(
         document.body.parentNode.$data = {};
         // Attach controllers.
         var W = require('x-widget');
-        W('wdg.gl03', 'wdg.gl0', {
+        W('wdg.gl02', 'wdg.gl0', {
             width: "240",
             height: "160"})
 
