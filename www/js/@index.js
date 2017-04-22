91e31063b0321155030e8eff5bfa9df9c2ee5ce2
Tolokoban <fabien.petitjean@free.fr>
2017-04-22 16:26:39 +0200

    ...

--- www/js/@index.js
+++ www/js/@index.js
@@ -8,7 +8,7 @@
 
  **********************************************************************/
 
-var require = function() {
+window.require = function() {
     var modules = {};
     var definitions = {};
     var nodejs_require = typeof window.require === 'function' ? window.require : null;
@@ -37,7 +37,7 @@ var require = function() {
         if (typeof mod === 'undefined') {
             mod = {exports: {}};
             var exports = mod.exports;
-            body(exports, mod);
+            body(f, mod, exports);
             modules[id] = mod.exports;
             mod = mod.exports;
             //console.log("Module initialized: " + id);
@@ -60,7 +60,7 @@ addListener(
         document.body.parentNode.$data = {};
         // Attach controllers.
         var W = require('x-widget');
-        W('wdg.gl025', 'wdg.gl0', {
+        W('wdg.gl013', 'wdg.gl0', {
             width: "240",
             height: "160"})
 
