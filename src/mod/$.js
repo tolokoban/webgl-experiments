a73900d81d2e346e54ab5d6e297d5f37eb07fab9
Fabien Petitjean <f.petitjean@lombardodier.com>
2018-09-10 16:39:53 +0200

    Ok

--- src/mod/$.js
+++ src/mod/$.js
@@ -1,4 +1,4 @@
-exports.config={"name":"\"webgl-experiments\"","description":"\"WebGL study by tutorials\"","author":"\"tolokoban\"","version":"\"0.0.103\"","major":"0","minor":"0","revision":"103","date":"2018-09-10T09:49:27.000Z","consts":{}};
+exports.config={"name":"\"webgl-experiments\"","description":"\"WebGL study by tutorials\"","author":"\"tolokoban\"","version":"\"0.0.104\"","major":"0","minor":"0","revision":"104","date":"2018-09-10T14:39:39.000Z","consts":{}};
 var currentLang = null;
 exports.lang = function(lang) {
     if (lang === undefined) {
