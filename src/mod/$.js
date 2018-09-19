a977b582181ce1849d840c13678b7f8aeaeb49d1
Tolokoban <fabien.petitjean@free.fr>
2018-09-19 22:15:08 +0200

    ...

--- src/mod/$.js
+++ src/mod/$.js
@@ -1,4 +1,4 @@
-exports.config={"name":"\"webgl-experiments\"","description":"\"WebGL study by tutorials\"","author":"\"tolokoban\"","version":"\"0.0.107\"","major":"0","minor":"0","revision":"107","date":"2018-09-19T20:07:44.000Z","consts":{}};
+exports.config={"name":"\"webgl-experiments\"","description":"\"WebGL study by tutorials\"","author":"\"tolokoban\"","version":"\"0.0.108\"","major":"0","minor":"0","revision":"108","date":"2018-09-19T20:13:56.000Z","consts":{}};
 var currentLang = null;
 exports.lang = function(lang) {
     if (lang === undefined) {
