20d83ac7e6318b83ca47be35c3d7530d55956e95
Fabien Petitjean <f.petitjean@lombardodier.com>
2019-01-24 17:16:10 +0100

    modification de la couleur de base.

--- src/mod/light-saber/colors.js
+++ src/mod/light-saber/colors.js
@@ -1,3 +1,4 @@
 "use strict";
 
-exports.PLASMA = new Float32Array( [ 1.0, 0.7, 0.2, 1.0 ] );
+//exports.PLASMA = new Float32Array( [ 1.0, 0.7, 0.2, 1.0 ] );
+exports.PLASMA = new Float32Array( [ 0.8, 0.4, 0.2, 1.0 ] );
