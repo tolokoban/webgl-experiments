c982870d10aec7ce516b23273ef8377e1407f859
Tolokoban <fabien.petitjean@free.fr>
2019-01-28 22:23:50 +0100

    On parle des coefficients binomiaux.

--- src/mod/canvas.js
+++ src/mod/canvas.js
@@ -1,8 +1,9 @@
 "use strict";
 
-var $ = require( "dom" );
-var DB = require( "tfw.data-binding" );
-var Resize = require( "webgl.resize" );
+const
+$ = require( "dom" ),
+DB = require( "tfw.data-binding" ),
+Resize = require( "webgl.resize" );
 
 module.exports = function ( slots ) {
   if( typeof slots.numbers === "undefined" ) slots.numbers = {};
@@ -14,22 +15,23 @@ module.exports = function ( slots ) {
       resolve();
     };
   }
-  if( typeof slots.draw !== "function" ) slots.draw = function() {};
+  if( typeof slots.draw !== "function" ) slots.draw = function() {
+    // Empty
+  };
 
-  var key;
-  var webgl = {
+  const webgl = {
     alpha: false,
     depth: false,
     stencil: false,
-    antialias: false,    
+    antialias: false,
     premultipliedAlpha: false,
     preserveDrawingBuffer: false,
     failIfMajorPerformanceCaveat: true
   };
-  for( key in slots.context ) {
-    webgl[key] = slots.context[key];    
+  for( let key in slots.context ) {
+    webgl[key] = slots.context[key];
   }
-  
+
   var constructor = function ( opts ) {
     var canvas = $.elem( this, 'canvas' );
 
@@ -42,11 +44,11 @@ module.exports = function ( slots ) {
       canvas.setAttribute( 'height', v );
       canvas.style.height = v + "px";
     } );
-    
+
     var defValues = { width: 640, height: 480 };
-    
+
     var key, val;
-    
+
     for( key in slots.strings ) {
       val = slots.strings[key];
       DB.propString( this, key );
@@ -67,11 +69,11 @@ module.exports = function ( slots ) {
 
     window.setTimeout( start.bind( this, canvas, webgl, slots ), 20 );
   };
-  
+
   return constructor;
 };
 
-  
+
 function start( canvas, webgl, slots ) {
   var gl = canvas.getContext( 'webgl', webgl );
   this.gl = gl;
